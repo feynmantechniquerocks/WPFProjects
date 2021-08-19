64b3bf783f9d582aa8ec1b9f3e6c25b2eab27817
mertkazanci <mertkazancii@gmail.com>
2021-08-20 01:06:07 +0300

    Sample app of the third post - WPF Combobox - The Drop-down list of WPF.

---
+++ [3] WPF Combobox/WPF Combobox - Sample Application/Ruler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Combobox___Sample_Application
+{
+    class Ruler
+    {
+        public string Name { get; set; }
+        public string reignName { get; set; }
+        public string House { get; set; }
+        public DateTime Birth { get; set; }
+
+        // Death DateTime can be nullable.
+        public DateTime? Death { get; set; }
+        public string Predecessor { get; set; }
+        public string Successor { get; set; }
+        public string Image_URL { get; set; }
+
+        // WPF ListBox uses the ToString() method of the class to decide what to display.
+        public override string ToString()
+        {
+            return reignName;
+        }
+    }
+}

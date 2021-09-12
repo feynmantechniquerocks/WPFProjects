cb6b28d96a8f2f2ad4140b3356686f8ea0be4312
mertkazanci <mertkazancii@gmail.com>
2021-09-13 00:07:41 +0300

    Prism Begins

---
+++ Prism Begins/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using Prism_Begins.Views;
+using System.Windows;
+
+namespace Prism_Begins
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}

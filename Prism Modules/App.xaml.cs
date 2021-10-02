077c9f9d860410a3e2c0b2f32194308a43b03794
mertkazanci <mertkazancii@gmail.com>
2021-10-02 15:46:03 +0300

    Prism Modules added.

---
+++ Prism Modules/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MyModule;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism_Modules.Views;
+using System.Windows;
+
+namespace Prism_Modules
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<MyModuleModule>();
+        }
+    }
+}
